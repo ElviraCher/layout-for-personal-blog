621b5ecea3d652a50cf4f089cbd968cc8098cdce
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-08-04 21:45:02 +0300

    Install webpack plugins for html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/script.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   output: {
     filename: "script.js",
     path: path.resolve(__dirname, "dist"),
@@ -12,6 +19,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
