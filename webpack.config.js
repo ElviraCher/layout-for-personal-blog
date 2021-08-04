3f406e81f4531f12943b0bab1723d5861ad1f921
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-08-04 21:26:41 +0300

    Fixing by eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/script.js",
-    output: {
-        filename: "script.js",
-        path: path.resolve(__dirname, "dist"),
-    },
+  entry: "./src/script.js",
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
+// todo: установить плагины для корректной сборки html и css
