7141889bb1102a870aa7d1e0e65eb79545db13d0
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-08-23 19:14:52 +0300

    Change folder name for pictures into dist

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext]",
+          filename: "img/[name][ext]",
         },
       },
     ],
