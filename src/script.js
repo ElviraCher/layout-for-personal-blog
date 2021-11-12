a3d155ae11cf28be3497ef2a6cd973edbb8c286f
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-11-12 19:51:05 +0300

    Add import html

--- src/script.js
+++ src/script.js
@@ -2,3 +2,5 @@ import "./styles/main.scss";
 // eslint-disable-next-line no-unused-vars
 import defaultExport from "./slider";
 import "./styles/slider.scss";
+// eslint-disable-next-line no-unused-vars
+import html from "./index.html";
