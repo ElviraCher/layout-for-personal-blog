98a28433154dda537dba42f15a80f862249a1490
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-11-17 21:28:56 +0300

    Delete unused import

--- src/script.js
+++ src/script.js
@@ -1,6 +1,2 @@
 import "./styles/main.scss";
-// eslint-disable-next-line no-unused-vars
-import defaultExport from "./slider";
 import "./styles/slider.scss";
-// eslint-disable-next-line no-unused-vars
-import html from "./index.html";
