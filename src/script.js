156066156f8d51cd6848c19c4abcfa2f8e7b65fa
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-23 16:58:09 +0300

    Add script for mobile header

--- src/script.js
+++ src/script.js
@@ -1,2 +1,15 @@
 import "./styles/main.scss";
 import "./styles/slider.scss";
+
+(function init() {
+  const iconMenu = document.querySelector(".menu__icon");
+
+  if (iconMenu) {
+    const menu = document.querySelector(".menu__body");
+    iconMenu.addEventListener("click", () => {
+      document.body.classList.toggle("no-scroll");
+      iconMenu.classList.toggle("menu__icon--active");
+      menu.classList.toggle("menu__body--active");
+    });
+  }
+})();
